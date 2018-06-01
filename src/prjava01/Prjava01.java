f7a3cba959833e81bb592cd56824999c1b03ac31
Oriol <Oriol@DESKTOP-JG0ND4M>
2018-06-01 17:19:29 +0200

    Versió 0.1 del projecte prjava01

---
+++ src/prjava01/Prjava01.java
@@ -0,0 +1,45 @@
+/*
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+ */
+package prjava01;
+
+/**
+ *
+ * @author orroma1718daw2vib (a on XYZ és el teu nom d'usuari Github)
+ */
+import java.io.*;
+
+public class Prjava01 {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");
+            bw.newLine();
+            bw.write("  <head>");
+            bw.newLine();
+            bw.write("    <title>");
+            bw.newLine();
+            bw.write("      Nova p&agrave;gina  web");
+            bw.newLine();
+            bw.write("    </title>");
+            bw.newLine();
+            bw.write("  </head>");
+            bw.newLine();
+            bw.write("  <body>");
+            bw.newLine();
+            bw.write("    Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write("  </body>");
+            bw.newLine();
+            bw.write("</html>");
+            bw.newLine();
+            bw.close();
+        }
+    }
+}
